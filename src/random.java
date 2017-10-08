00262df1006c7c35be89bb3d3441e53443d19d66
Chen <kitaice@users.noreply.github.com>
2017-10-07 21:35:51 -0500

    Add files via upload

--- src/random.java
+++ src/random.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.text.DecimalFormat;
 //import java.util.Random;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class random {
     public static ArrayList<ArrayList<Double>> create(FileWriter writer, int size, ArrayList<ArrayList<Double>> train )throws IOException {
